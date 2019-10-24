df57026bd105845cca4c77cd492227443969ed76
David Dernovoj <david@dernovoj.com>
2019-10-24 21:38:55 +0200

    Updated the run script to install deps

--- run.sh
+++ run.sh
@@ -1,1 +1,22 @@
-npm run start
+#!/bin/sh
+
+
+if [ ! -d "node_modules" ]
+then
+    # Install dependencies and create a file indicating successful installation
+    npm install && touch node_modules/.all-deps-installed
+fi
+
+if [ ! -d "tmp" ]
+then
+    mkdir tmp
+fi
+
+# Wait until dependencies have been installed
+while [ ! -f node_modules/.all-deps-installed ]
+do
+    echo "Waiting for dependencies to be installed..."
+    sleep 5
+done
+
+exec npm run start
